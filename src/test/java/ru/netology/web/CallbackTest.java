d9bc59f7d23e99f9cbbc5c9291adc137a7642ed9
Aleksandr Petrenko <aleksandr_petrenko62@mail.ru>
2023-06-26 21:00:52 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,15 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CallbackTest {
+
+    @Test
+    void shouldTest() {
+        
+
+    }
+
+}
