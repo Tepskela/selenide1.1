0a7f1b6d2b35bab190c598f57892ea8d83faf885
Aleksandr Petrenko <aleksandr_petrenko62@mail.ru>
2023-06-28 13:40:30 +0300

    GoodTest

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,15 +1,23 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CallbackTest {
 
     @Test
     void shouldTest() {
-
-
+        open ("http://localhost:9999/");
+        SelenideElement form = $("[class=App_appContainer__3jRx1]");
+        //SelenideElement form = $("[class=form form_size_m form_theme_alfa-on-white]");
+        form.$("[data-test-id=name] input").setValue("Александр");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[class=button__content]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
-
 }
